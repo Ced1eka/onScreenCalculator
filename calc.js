4f1e649e040456c9587345348c16f0edb34379a1
Ced <cedieka2@gmail.com>
2023-09-20 06:26:13 +0900

    Created operand functions and variables to store them.

---
+++ calc.js
@@ -0,0 +1,8 @@
+let numberOne = 0;
+let numberTwo = 0;
+let Operand;
+
+const ADD = () => { return Number(numberOne + numberTwo)};
+const SUBT = () => { return Number(numberOne - numberTwo)};
+const MULT = () => { return Number(numberOne * numberTwo)};
+const DIVI = () => { return Number(numberOne / numberTwo)};
