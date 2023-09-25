e7fdeb82d69095453e42a3850a95326137c5dbe5
Ced <cedieka2@gmail.com>
2023-09-26 02:18:06 +0900

    Added styles for the buttons and calculator itself.
    After making the basic structure, make sure if
    it's using flexbox right.

--- calc.js
+++ calc.js
@@ -6,3 +6,15 @@ const ADD = () => { return Number(numberOne + numberTwo)};
 const SUBT = () => { return Number(numberOne - numberTwo)};
 const MULT = () => { return Number(numberOne * numberTwo)};
 const DIVI = () => { return Number(numberOne / numberTwo)};
+
+
+let aButton =document.getElementsByTagName("button");
+
+// Loop through all button elements and add a click event listener to each
+for (let i = 0; i < aButton.length; i++) {
+      aButton[i].addEventListener('click', function() {
+        // Access the data-value attribute of the clicked button
+        const dataValue = this.getAttribute('data-value');
+        console.log(dataValue);
+      });
+    }
