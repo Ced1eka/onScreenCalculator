3c3996fd795d2832883bbb2d128ccb6df533abf0
Ced <cedieka2@gmail.com>
2023-09-30 04:42:46 +0900

    Added clear button functionality and misc. code fixes.

--- calc.js
+++ calc.js
@@ -1,5 +1,5 @@
-let numberOne = 0;
-let numberTwo = 0;
+let numberOne = [];
+let numberTwo = [];
 let Operand;
 
 const ADD = () => { return Number(numberOne + numberTwo)};
@@ -8,13 +8,29 @@ const MULT = () => { return Number(numberOne * numberTwo)};
 const DIVI = () => { return Number(numberOne / numberTwo)};
 
 
-let aButton =document.getElementsByTagName("button");
+let aButton =document.getElementsByClassName("btn");
+
+const screenDisp = document.getElementById("outputs");
+
 
 // Loop through all button elements and add a click event listener to each
 for (let i = 0; i < aButton.length; i++) {
       aButton[i].addEventListener('click', function() {
         // Access the data-value attribute of the clicked button
         const dataValue = this.getAttribute('data-value');
-        console.log(dataValue);
+        screenDisp.textContent += dataValue;
+
+        numberOne += `${dataValue}`;
+        console.log(numberOne);
+
       });
     }
+
+const clearValues = () => {
+  screenDisp.textContent = '';
+  numberOne = [];
+  // console.log(numberOne);
+
+};
+const clearBtn = document.querySelector('.clear');
+clearBtn.addEventListener('click', clearValues);
